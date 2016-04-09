eda4b8bc5b4c2b98e8ff14a1816b1f43a05f058a
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-04-09 16:31:27 +0200

    [TASK] Update TYPO3 dependency + clean up

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,26 +1,26 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-  'title' => 'Plain PHP based Data Display (Data Consumer) - Tesseract project',
-  'description' => 'Use Plain PHP templates to display any kind of data returned by a Data Provider. More info on http://www.typo3-tesseract.com',
-  'category' => 'fe',
-  'author' => 'Fabien Udriot',
-  'author_email' => 'fabien.udriot@ecodev.ch',
-  'shy' => '',
-  'state' => 'stable',
-  'version' => '1.4.0',
-  'constraints' =>
-  [
-    'depends' =>
-    [
-      'typo3' => '4.5.0-6.2.99',
-      'tesseract' => '1.7.0-0.0.0',
-    ],
-    'conflicts' =>
-    [
-    ],
-    'suggests' =>
-    [
-    ],
-  ],
+    'title' => 'Plain PHP based Data Display (Data Consumer) - Tesseract project',
+    'description' => 'Use Plain PHP templates to display any kind of data returned by a Data Provider. More info on http://www.typo3-tesseract.com',
+    'category' => 'fe',
+    'author' => 'Fabien Udriot',
+    'author_email' => 'fabien.udriot@ecodev.ch',
+    'shy' => '',
+    'state' => 'stable',
+    'version' => '1.4.0',
+    'constraints' =>
+        [
+            'depends' =>
+                [
+                    'typo3' => '7.6.0-7.99.99',
+                    'tesseract' => '1.7.0-0.0.0',
+                ],
+            'conflicts' =>
+                [
+                ],
+            'suggests' =>
+                [
+                ],
+        ],
 ];
